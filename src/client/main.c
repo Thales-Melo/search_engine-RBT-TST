4fa436e6d03bc0b48bab952f6f8c007201d58c99
RenanCampista <renan.campista@edu.ufes.br>
2024-09-06 10:12:04 -0300

    feat: add debug mode
    Esse modo imprime o tempo de execução do programa. Instruções no README.

--- src/client/main.c
+++ src/client/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <time.h>
+
 #include "../libs/include/stop_word.h"
 #include "../libs/include/string_map.h"
 #include "../libs/include/page_map.h"
@@ -8,6 +10,12 @@
 int main(int argc, char *argv[]) {
     if (argc != 2)
         exit(printf("Error: invalid number of arguments.\n"));
+
+    #ifdef DEBUG_MODE
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+    #endif
 
     StopWord *stop_words = stop_word_read(argv[1]);
     StringMap *sm = string_map_build(argv[1], stop_words);
@@ -17,5 +25,11 @@ int main(int argc, char *argv[]) {
     string_map_destruct(sm);
     page_map_destruct(pm);
     
+    #ifdef DEBUG_MODE
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Execution time: %f seconds\n", cpu_time_used);
+    #endif
+
     return 0;
 }
